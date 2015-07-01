8b5607d130cbfbd6d8bbb440b28a90cfacec0e0e
JD <jdstep23@gmail.com>
2015-06-30 20:22:19 -0700

    buys 10% of current shares

--- client/app/views/WalletView.js
+++ client/app/views/WalletView.js
@@ -12,6 +12,10 @@ var WalletView = Backbone.View.extend({
   initialize: function() {
     this.model.on('change', this.render, this);
     this.render();
+    // DEBUG VALUES BEFORE HOOKING UP WITH REAL STOCKS
+    this.nShares = 200;
+    this.adjClose = 5;
+    this.magnitudeBuySell = .2;
   },
 
   events: {
@@ -28,14 +32,24 @@ var WalletView = Backbone.View.extend({
   handleBuy: function(event) {
     event.preventDefault();
 
-    var nShares = 100;
-    var adjClose = 2;
-
-    // console.log('you tried to buy!');
+    var nShares = this.nShares;
+    var adjClose = this.adjClose;
     var currentCash = this.model.get('cash');
 
-    this.model.set('cash', currentCash+5);
-    console.log(this.model.get('cash'));
+    // debugger;
+    var numSharesToBuy = nShares * this.magnitudeBuySell;
+    var cost = numSharesToBuy * adjClose;
+    console.log('num shares to buy', numSharesToBuy);
+    // console.log('you tried to buy!');
+
+    if (currentCash - cost < 0) {
+      console.error('ERROR: trying to spend more cash than you have');
+      return;
+    }
+    this.model.set('cash', currentCash-cost);
+    this.nShares = this.nShares + numSharesToBuy;
+    console.log('new number of shares', this.nShares);
+
   },
 
   handleSell: function(event) {
@@ -45,7 +59,7 @@ var WalletView = Backbone.View.extend({
 
     var currentCash = this.model.get('cash');
 
-    this.model.set('cash', currentCash-5);
+    this.model.set('cash', currentCash+5);
     console.log(this.model.get('cash'));
 
   }
