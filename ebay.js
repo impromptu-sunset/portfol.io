a25c2ed42a6d223599bbdc70d9fcfc0f051690cf
JD <jdstep23@gmail.com>
2015-07-01 17:30:34 -0700

    ebay API variables set up

--- ebay.js
+++ ebay.js
@@ -23,7 +23,8 @@ var categories = {
   island: {
     min: 1000000,
     max: 3000000,
-    id: 15841
+    id: 15841,
+    keyword: 'island'
   },
   boat: {
     min: 5000,
@@ -64,5 +65,27 @@ var getEbayProductCategory = function(cost) {
 };
 
 var getEbayProduct = function(category) {
+  var maxPrice = category.max;
+  var categoryId = category.id;
+  var keyword = category.keyword || null;
 
+  var requestUrl = 
+          'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced\
+          &SERVICE-VERSION=1.12.0\
+          &SECURITY-APPNAME=Jonathan-3653-441d-a058-edbde13c5f5c\
+          &RESPONSE-DATA-FORMAT=JSON\
+          &REST-PAYLOAD\
+          &paginationInput.entriesPerPage=1\
+          &categoryId=' + categoryId +'\
+          &sortOrder=CurrentPriceHighest\
+          &itemFilter(0).name=MaxPrice\
+          &itemFilter(0).value=' + MaxPrice +'\
+          '
+ ;
+  if (keyword) {
+    requestUrl += '&keywords=' + keyword);
+  }
+  request(requestUrl, function(error, res, data)) {
+
+  }
 };
