6601c470009719a7948add091b1a9ed4b30ef810
JD <jdstep23@gmail.com>
2015-07-03 11:16:49 -0700

    ebay and random item added for one value

--- client/app/views/ResultView.js
+++ client/app/views/ResultView.js
@@ -3,10 +3,10 @@ var ResultView = Backbone.View.extend({
     this.render();
   },
 
+  template: _.template('<h5><%= ebay.title[0] %></h5><img src="<%= ebay.galleryURL %>" />' +
+                       '<h5><%= randomItem.quantity %> <%= randomItem.description %>'),
+
   render: function(){
-    console.log('trying to render result view');
-    console.log('result view model', this.model);
-    this.$el.html('<p>hello from result view</p>')
-    // return this.$el.html(this.template(this.model.attributes));
+    return this.$el.html(this.template(this.model.attributes));
   }
 });
